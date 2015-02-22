976cc31d299773b7a024676cee7b5ebb518fc9c8
Dan Shawver <dshawver@villanova.edu>
2015-02-22 14:32:12 -0500

    Revert "Modified home page to reflect basic wireframe.  Added 'Report a Sexual Assault', 'Talk to Someone', 'Get Medical Help', and 'Get Information' pages."
    This reverts commit b648f0c0ec42d5d9a17034c2235cf41ef2c5315a.

--- SAVUR/www/js/controllers.js
+++ SAVUR/www/js/controllers.js
@@ -39,7 +39,6 @@ angular.module('starter.controllers', [])
     $scope.slideIndex = index;
   };
 })
-
 .controller('HomeCtrl',function($scope, $ionicPopup, $timeout) {
 
  // A confirm dialog
@@ -55,29 +54,13 @@ angular.module('starter.controllers', [])
        console.log('You are not sure');
      }
    });
- }; 
-})
-
-.controller('ReportAssaultCtrl',function($scope) {
-	
-})
-
-.controller('TalkCtrl',function($scope) {
-	
-})
-
-.controller('MedicalHelpCtrl',function($scope) {
-	
-})
-
-.controller('InformationCtrl',function($scope) {
-	
+ };
 })
 
 .controller('ProfileCtrl', function($scope){
       $scope.profileName = window.localStorage['profileName'] ;
-      $scope.profileVUID =  window.localStorage['profileVUID'] ;
-      $scope.profileDorm =  window.localStorage['profileDorm'] ;
+    $scope. profileVUID =  window.localStorage['profileVUID'] ;
+    $scope. profileDorm =  window.localStorage['profileDorm'] ;
 });
 
 ;
