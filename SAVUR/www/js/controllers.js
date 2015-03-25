7241537e250f075668f02658b11eb17e4d5d4f20
Kevin Yeom <kevin@sooryen.com>
2015-03-24 23:01:31 -0400

    logo added

--- SAVUR/www/js/controllers.js
+++ SAVUR/www/js/controllers.js
@@ -60,7 +60,33 @@ angular.module('starter.controllers', [])
 })
 
 .controller('ReportAssaultCtrl',function($scope) {
+
+  $scope.test = "abc";
+
+  $scope.sendtext = function(){
+         var number = document.getElementById('numberTxt').value;
+        var message = document.getElementById('messageTxt').value;
+        alert(number);
+        alert(message);
+
+        //CONFIGURATION
+        var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
+            }
+        };
+
+        var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
+        sms.send(number, message, options, success, error);
+  }
+
 	
+
+
+
 })
 
 .controller('TalkCtrl',function($scope) {
