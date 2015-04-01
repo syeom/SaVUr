7ec07f62376693b4b4ce1b4445626317708106b6
Kevin Yeom <kevin@sooryen.com>
2015-03-31 21:25:11 -0400

    added plugin

---
+++ SAVUR/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/de.appplant.cordova.plugin.email-composer/www/email_composer.js",
+        "id": "de.appplant.cordova.plugin.email-composer.EmailComposer",
+        "clobbers": [
+            "cordova.plugins.email",
+            "plugin.email"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "de.appplant.cordova.plugin.email-composer": "0.8.2"
+}
+// BOTTOM OF METADATA
+});
