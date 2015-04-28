421beee61d54fa5d01a71cb3e1d642c484b31a74
Dan Shawver <dshawver@villanova.edu>
2015-04-28 16:15:56 -0400

    All links to external websites now use Cordova InAppViewer to route to the native browser. This is a much better option than iframes and allows the user to zoom and get back to the actual app.  Also did some clean-up of files and removed some files that were not needed/being used.

--- SAVUR/www/js/controllers.js
+++ SAVUR/www/js/controllers.js
@@ -182,30 +182,8 @@ if (window.localStorage['profileName'] == null && window.localStorage['profileNa
 })
 
 
-.controller('MedicalHelpCtrl',function($scope,$state) {
-	     $scope.getDirections = function(lat,lng){
-            window.localStorage['mapLat'] = lat;
-            window.localStorage['mapLong'] = lng;
-            $state.go('app.map');
-        };
+.controller('MedicalHelpCtrl',function($scope) {
 })
-
-.controller('MapCtrl', function($scope) {
-        //alert("Hola");
-           var myLatlng = new google.maps.LatLng(window.localStorage['mapLat'],window.localStorage['mapLong']);
-           var mapOptions = {
-              center: myLatlng,
-              zoom: 16,
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            var map = new google.maps.Map(document.getElementById("map"),
-                mapOptions);
-             var marker = new google.maps.Marker({
-                position: myLatlng,
-                map: map,
-                title: 'Bryn Mawr Hospital'
-        });
-       })
 
 
 .controller('InformationCtrl',function($scope) {
@@ -231,50 +209,6 @@ if (window.localStorage['profileName'] == null && window.localStorage['profileNa
 .controller('SexualAssaultPamphletCtrl',function($scope, $ionicSlideBoxDelegate) {
     
 })
-
-      //   //Marker + infowindow + angularjs compiled ng-click
-      //   var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
-      //   var compiled = $compile(contentString)($scope);
-
-      //   var infowindow = new google.maps.InfoWindow({
-      //     content: compiled[0]
-      //   });
-
-      //   var marker = new google.maps.Marker({
-      //     position: myLatlng,
-      //     map: map,
-      //     title: 'Uluru (Ayers Rock)'
-      //   });
-
-      //   google.maps.event.addListener(marker, 'click', function() {
-      //     infowindow.open(map,marker);
-      //   });
-
-      //   $scope.map = map;
-
-      // google.maps.event.addDomListener(window, 'load', initialize);
-      
-      // $scope.centerOnMe = function() {
-      //   if(!$scope.map) {
-      //     return;
-      //   }
-
-      //   $scope.loading = $ionicLoading.show({
-      //     content: 'Getting current location...',
-      //     showBackdrop: false
-      //   });
-
-      //   navigator.geolocation.getCurrentPosition(function(pos) {
-      //     $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      //     $scope.loading.hide();
-      //   }, function(error) {
-      //     alert('Unable to get location: ' + error.message);
-      //   });
-      // };
-      
-      // $scope.clickTest = function() {
-      //   alert('Example of infowindow with ng-click')
-      // };
 
 .controller('ProfileCtrl', function($scope){
 
